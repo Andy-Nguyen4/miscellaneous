6cca256bf00a8e125a7853538075a0d42740bbc1
Andy-Nguyen4 <andynguyen1541@gmail.com>
2024-07-18 21:38:25 -0400

    Created web page with animation

---
+++ animation/js/animation.js
@@ -0,0 +1,22 @@
+function myMove() {
+    let id = null;
+    const el = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 450) {
+            clearInterval(id);
+        } else {
+            pos++;
+            el.style.top = pos + "px";
+            el.style.left = pos + "px";
+        }
+    }
+}
+
+$(document).ready(function() {
+    $("#move").on("click", function(){
+        myMove();
+    });
+});
